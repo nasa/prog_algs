ceb9862c2adf1f874f8fe8aad8829e7c06a0e83c
Christopher Teubert <christopher.a.teubert@nasa.gov>
2020-11-06 16:36:50 -0800

    First Draft Particle Filter

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.insert(1, "/Users/cteubert/Desktop/python-prognostics-models-package/")
 from prog_models.models import battery_circuit
-from prog_algs.state_estimators import unscented_kalman_filter
+from prog_algs.state_estimators import unscented_kalman_filter, particle_filter
 
 ## Setup
 def future_loading(t):
@@ -19,19 +21,27 @@ def future_loading(t):
 batt = battery_circuit.BatteryCircuit()
 
 ## State Estimation - perform a single ukf state estimate step
-ukf = unscented_kalman_filter.UnscentedKalmanFilter(batt, future_loading, batt.parameters['x0'])
-print("Prior State:", ukf.x, '\n\tlog-likelihood', ukf.filter.log_likelihood)
-print('\tSOC: ', batt.event_state(ukf.t, ukf.x)['EOD'])
-ukf.estimate(0.1, {'t': 32.2, 'v': 3.915})
-print("Posterior State:", ukf.x, '\n\tlog-likelihood', ukf.filter.log_likelihood)
-print('\tSOC: ', batt.event_state(ukf.t, ukf.x)['EOD'])
+
+# filt = unscented_kalman_filter.UnscentedKalmanFilter(batt, future_loading, batt.parameters['x0'])
+filt = particle_filter.ParticleFilter(batt, future_loading, batt.parameters['x0'])
+
+print("Prior State:", filt.x)
+print('\tSOC: ', batt.event_state(filt.t, filt.x)['EOD'])
+filt.estimate(0.1, {'t': 32.2, 'v': 3.915})
+print("Posterior State:", filt.x)
+print('\tSOC: ', batt.event_state(filt.t, filt.x)['EOD'])
 
 ## Prediction - Predict EOD given current state
 from prog_algs.predictors import monte_carlo
 from prog_algs import samplers
 mc = monte_carlo.MonteCarlo(batt)
-state_sampler = samplers.generate_mean_cov_random_sampler(batt.states, list(ukf.x.values()), ukf.Q)
-prediction_config = {'dt': 0.025, 'num_samples':5}
+if isinstance(filt, unscented_kalman_filter.UnscentedKalmanFilter):
+    state_sampler = samplers.generate_mean_cov_random_sampler(batt.states, list(filt.x.values()), filt.Q)
+    prediction_config = {'dt': 0.025, 'num_samples':5}
+else:
+    def state_sampler(num_samples):
+        return filt.particles
+    prediction_config = {'dt': 0.025, 'num_samples':len(filt.particles)}
 (times, inputs, states, outputs, event_states, eol) = mc.predict(state_sampler, future_loading, prediction_config)
 
 # Print Prediction Results

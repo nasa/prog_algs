3e5e4bae81a3857409ba06f78f4633b00daf52f1
Christopher Teubert <christopher.a.teubert@nasa.gov>
2020-11-11 12:39:19 -0800

    Moved metrics into samples file

--- example.py
+++ example.py
@@ -48,7 +48,7 @@ else:
 
 # Print Metrics
 print("\nEOD Predictions (s):")
-from prog_algs import metrics
+from prog_algs.metrics import samples as metrics 
 print('\t', metrics.eol_stats(eol))
 print('\tPercentage between 3005.2 and 3005.6: ', metrics.percentage_in_bounds(eol, [3005.2, 3005.6])*100.0, '%')
 print('\tAssuming ground truth 3002.25: ', metrics.eol_stats(eol, 3005.25))
--- prog_algs/metrics/__init__.py
+++ prog_algs/metrics/__init__.py
@@ -1,37 +1,4 @@
 # Copyright © 2020 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
-from timeit import timeit
+from . import samples
 
-def eol_stats(rul, ground_truth = None):
-    rul.sort()
-    mean = sum(rul)/len(rul)
-    median = rul[int(len(rul)/2)]
-    stats = {
-        'min': rul[0],
-        'percentiles': {
-            '0.01': rul[int(len(rul)/10000)] if len(rul) >= 10000 else None,
-            '0.1': rul[int(len(rul)/1000)] if len(rul) >= 1000 else None,
-            '1': rul[int(len(rul)/100)] if len(rul) >= 100 else None,
-            '10': rul[int(len(rul)/10)] if len(rul) >= 10 else None,
-            '25': rul[int(len(rul)/4)] if len(rul) >= 4 else None,
-            '50': median,
-            '75': rul[int(3*len(rul)/4)] if len(rul) >= 4 else None,
-        },
-        'median': median,
-        'mean': mean,
-        'max': rul[-1],
-        'median absolute deviation': sum([abs(x - median) for x in rul])/len(rul),
-        'mean absolute deviation':   sum([abs(x - mean)   for x in rul])/len(rul),
-        'number of samples': len(rul)
-    }
-
-    if ground_truth is not None:
-        # Metrics comparing to ground truth
-        stats['mean absolute error'] = sum([abs(x - ground_truth) for x in rul])/len(rul)
-        stats['mean absolute percentage error'] = stats['mean absolute error']/ ground_truth
-        #TODO(CT): More, percentile?
-
-    return stats
-
-def percentage_in_bounds(rul, bounds):
-    return sum([x < bounds[1] and x > bounds[0] for x in rul])/ len(rul)

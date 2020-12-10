cb40f6d457bc1e5d8ae1425f2c27856e4c17495f
Christopher Teubert <christopher.a.teubert@nasa.gov>
2020-12-10 08:35:30 -0800

    Fix benchmarking example

--- prog_algs/__init__.py
+++ prog_algs/__init__.py
@@ -7,25 +7,26 @@ from .uncertain_data import *
 
 import numpy as np
 
-def run_prog_playback(obs, pred, state_sampler, future_loading, output_measurements, options = {}):
+def run_prog_playback(obs, pred, future_loading, output_measurements, options = {}):
     config = {# Defaults
         'predict_rate': 0, # Default- predict every step
+        'num_samples': 10,
         'predict_config': {}
     }
     config.update(options)
 
     next_predict = output_measurements[0][0] + config['predict_rate']
-    times = np.empty((len(output_measurements), config['predict_config']['num_samples']), dtype=object)
-    inputs = np.empty((len(output_measurements), config['predict_config']['num_samples']), dtype=object)
-    states = np.empty((len(output_measurements), config['predict_config']['num_samples']), dtype=object)
-    outputs = np.empty((len(output_measurements), config['predict_config']['num_samples']), dtype=object)
-    event_states = np.empty((len(output_measurements), config['predict_config']['num_samples']), dtype=object)
-    eols = np.empty((len(output_measurements), config['predict_config']['num_samples']), dtype=object)
+    times = np.empty((len(output_measurements), config['num_samples']), dtype=object)
+    inputs = np.empty((len(output_measurements), config['num_samples']), dtype=object)
+    states = np.empty((len(output_measurements), config['num_samples']), dtype=object)
+    outputs = np.empty((len(output_measurements), config['num_samples']), dtype=object)
+    event_states = np.empty((len(output_measurements), config['num_samples']), dtype=object)
+    eols = np.empty((len(output_measurements), config['num_samples']), dtype=object)
     index = 0
     for (t, measurement) in output_measurements:
-        obs.estimate(t, measurement)
+        obs.estimate(t, future_loading(t), measurement)
         if t >= next_predict:
-            (t, u, x, z, es, eol) = pred.predict(state_sampler, future_loading, config['predict_config'])
+            (t, u, x, z, es, eol) = pred.predict(obs.x.sample(config['num_samples']), future_loading, config['predict_config'])
             times[index, :] = t
             inputs[index, :]  = u
             states[index, :]  = x

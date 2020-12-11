1ae1ea4b447e43b167edf86c49be2a7190a35f6c
Christopher Teubert <christopher.a.teubert@nasa.gov>
2020-12-11 09:43:39 -0800

    Fix documentation

--- sphinx_config/conf.py
+++ sphinx_config/conf.py
@@ -40,7 +40,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-autodoc_mock_imports = ['numpy', 'filtpy']
+autodoc_mock_imports = ['numpy', 'scipy', 'filtpy']
 
 
 # -- Options for HTML output -------------------------------------------------

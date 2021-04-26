70d6a3ec75337f99840915b240654bce15dcb67d
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-04-26 10:57:10 -0700

    shortened description

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name = 'prog_algs',
     version = '0.3.0',
-    description = "The NASA Prognostic Algorithm Package is a python framework for model-based prognostics (computation of remaining useful life) of engineering systems, and provides a set of algorithms for state estimation and prediction, including uncertainty propagation. The algorithms take as inputs prognostic models (from NASA's Prognostics Model Package), and perform estimation and prediction functions. The library allows the rapid development of prognostics solutions for given models of components and systems. Different algorithms can be easily swapped to do comparative studies and evaluations of different algorithms to select the best for the application at hand.",
+    description = "The NASA Prognostic Algorithm Package is a framework for model-based prognostics (computation of remaining useful life) of engineering systems. It includes algorithms for state estimation and prediction, including uncertainty propagation. The algorithms use prognostic models (see prog_models) to perform estimation and prediction. The package enables rapid development of prognostics solutions for given models of components and systems. Algorithms can be swapped for comparative studies and evaluations",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url = 'https://github.com/nasa/prog_algs',
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only'
-    ]
+    ],
     keywords = ['prognostics', 'diagnostics', 'fault detection', 'fdir', 'prognostics and health management', 'PHM', 'health management'],
     packages = ['prog_algs', 'prog_algs.metrics', 'prog_algs.state_estimators', 'prog_algs.uncertain_data', 'prog_algs.visualize'],
     python_requires='>=3.6, <3.9',

39645c516d08d327d6c57f57b1a9574b2c5c18a8
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-09-22 15:26:09 -0700

    Add description of use to basic_example

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -36,8 +36,21 @@ def run_example():
         samples = filt.x.sample(20)
     else: # Particle Filter
         samples = filt.x.raw_samples()
+
     # Predict with a step size of 0.1
     (times, inputs, states, outputs, event_states, eol) = mc.predict(samples, future_loading, dt=0.1)
+
+    # The results of prediction can be accessed by sample, e.g.,
+    times_sample_1 = times[1]
+    states_sample_1 = states[1]
+    # now states_sample_1[n] corresponds to time_sample_1[n]
+    # you can also plot the results (state_sample_1.plot())
+
+    # You can also access a state at a specific time using the .snapshot function
+    states_time_1 = states.snapshot(1)
+    time1 = times.snapshot(1)
+    # now you have all the samples from a specific time (time1)
+    
     ## Print Metrics
     print("\nEOD Predictions (s):")
     from prog_algs.metrics import samples as metrics 

51eb7730b943918902acf3ccbf6592aabf70b198
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-09-22 15:23:49 -0700

    Added prediction class

---
+++ src/prog_algs/predictors/prediction.py
@@ -0,0 +1,62 @@
+from collections import UserList
+from ..uncertain_data import UnweightedSamples
+
+
+class Prediction(UserList):
+    """
+    Result of a prediction
+    """
+    __slots__ = ['times', 'data']  # Optimization 
+
+    def __init__(self, times, data):
+        """
+        Args:
+            times (array(float)): Times for each data point where times[n] corresponds to data[n]
+            data (array(dict)): Data points where data[n] corresponds to times[n]
+        """
+        self.times = times
+        self.data = data
+
+    def __eq__(self, other):
+        """Compare 2 Predictions
+
+        Args:
+            other (Precition)
+
+        Returns:
+            bool: If the two Predictions are equal
+        """
+        return self.times == other.times and self.data == other.data
+
+    def sample(self, id):
+        """Get sample by sample_id, equivalent to prediction[id]
+
+        Args:
+            id (int): Id of sample
+
+        Returns:
+            SimResult: Values for that sample at different times where result[i] corresponds to time[i]
+        """
+        return self[id]
+
+    def snapshot(self, index):
+        """Get all samples from a specific timestep
+
+        Args:
+            index (int): Timestep (index number from times)
+
+        Returns:
+            UnweightedSamples: Samples for time corresponding to times[timestep]
+        """
+        return UnweightedSamples([sample[index] for sample in self.data])
+
+    def time(self, index):
+        """Get time for data point at index `index`
+
+        Args:
+            index (int)
+
+        Returns:
+            float: Time for which the data point at index `index` corresponds
+        """
+        return self.times[index]

99e0a0162bb0de2306525474920f4eb6cb2e1a56
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-09-29 16:26:42 -0700

    Unscented Kalman Predictor & Example

--- src/prog_algs/predictors/__init__.py
+++ src/prog_algs/predictors/__init__.py
@@ -1,5 +1,7 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
-__all__ = ['predictor', 'monte_carlo']
+__all__ = ['predictor', 'monte_carlo', 'unscented_kalman_predictor']
 from .monte_carlo import MonteCarlo
 from .predictor import Predictor
+from .prediction import Prediction
+from .unscented_kalman_predictor import UnscentedKalmanPredictor

9789d9386f4d0e876ffe0e820e13ee0879682921
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-10-01 17:06:16 -0700

    fix

--- src/prog_algs/uncertain_data/multivariate_normal_dist.py
+++ src/prog_algs/uncertain_data/multivariate_normal_dist.py
@@ -1,6 +1,7 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration. All Rights Reserved.
 
 from . import UncertainData
+from numpy import array
 from numpy.random import multivariate_normal
 
 import warnings

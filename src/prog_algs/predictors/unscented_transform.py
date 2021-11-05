0cce3f44bff01af9c862cea7c4f2611b4c3ab130
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-11-04 17:02:24 -0700

    Remove final (not sure where that came from)

--- src/prog_algs/predictors/unscented_transform.py
+++ src/prog_algs/predictors/unscented_transform.py
@@ -1,6 +1,5 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
-from typing import final
 from .prediction import MultivariateNormalDistPrediction, UnweightedSamplesPrediction
 from .predictor import Predictor
 from numpy import diag, array, transpose

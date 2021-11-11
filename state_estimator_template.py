9f1be78e271eaf548c8688ef2b84c42cbbc842b1
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-11-11 14:31:39 -0800

    Update state estimator template

--- state_estimator_template.py
+++ state_estimator_template.py
@@ -1,6 +1,9 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
 from prog_algs import state_estimators
+# Replace the following with whatever form of UncertainData you would like to use to represent state 
+from prog_algs.uncertain_data import UncertainData, ScalarData
+
 
 class TemplateStateEstimator(state_estimators.StateEstimator):
     """
@@ -14,7 +17,7 @@ class TemplateStateEstimator(state_estimators.StateEstimator):
 
     def __init__(self, model, x0, measurement_eqn=None, **kwargs):
         """
-        Constructor
+        Constructor (optional)
         """
         super().__init__(model, x0, measurement_eqn, **kwargs)
         # ADD PARAMETER CHECKS HERE
@@ -22,7 +25,7 @@ class TemplateStateEstimator(state_estimators.StateEstimator):
 
         # ADD ANY STATE ESTIMATOR INITIALIZATION LOGIC
 
-    def estimate(self, t, u, z):
+    def estimate(self, t, u, z) -> None:
         """
         Perform one state estimation step (i.e., update the state estimate)
 
@@ -32,7 +35,7 @@ class TemplateStateEstimator(state_estimators.StateEstimator):
             Current timestamp in seconds (≥ 0.0)
             e.g., t = 3.4
         u : dict
-            Measured inputs, with keys defined by model.inputs.
+            Applied inputs, with keys defined by model.inputs.
             e.g., u = {'i':3.2} given inputs = ['i']
         z : dict
             Measured outputs, with keys defined by model.outputs.
@@ -40,8 +43,10 @@ class TemplateStateEstimator(state_estimators.StateEstimator):
         """
         # REPLACE WITH UPDATE STATE ESTIMATION
 
+        # Note, returns none. State is accessed using the property state_estimator.x
+
     @property
-    def x(self):
+    def x(self) -> UncertainData:
         """
         Getter for property 'x', the current estimated state. 
 
@@ -50,6 +55,8 @@ class TemplateStateEstimator(state_estimators.StateEstimator):
         state = observer.x
         """
         # REPLACE THE FOLLOWING WITH THE LOGIC TO CONSTRUCT/RETURN THE STATE
-        x = {key: 0.0 for key in self.model.states}
+
+        # Here we're using ScalarData, but the state could be represented by any other type of UncertainData (e.g., MultivariateNormalDist)
+        x = ScalarData({key: 0.0 for key in self.model.states})
 
         return x

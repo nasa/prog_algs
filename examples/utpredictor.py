22a8cedbe908d3c2d634f2609bce5183097d7ae9
Lawrence Hwang <lawrenceh326@gmail.com>
2022-02-24 13:57:55 -0800

    utpredictor updated with namedtuple syntax

--- examples/utpredictor.py
+++ examples/utpredictor.py
@@ -40,20 +40,20 @@ def run_example():
     mc = predictors.UnscentedTransformPredictor(batt)
 
     # Predict with a step size of 0.1
-    (times, inputs, states, outputs, event_states, toe) = mc.predict(filt.x, future_loading, dt=0.1, save_freq= 100)
+    prediction_results = mc.predict(filt.x, future_loading, dt=0.1, save_freq= 100)
 
     # Print Results
-    for i, time in enumerate(times):
+    for i, time in enumerate(prediction_results.times):
         print('\nt = {}'.format(time))
-        print('\tu = {}'.format(inputs.snapshot(i).mean))
-        print('\tx = {}'.format(states.snapshot(i).mean))
-        print('\tz = {}'.format(outputs.snapshot(i).mean))
-        print('\tevent state = {}'.format(event_states.snapshot(i).mean))
+        print('\tu = {}'.format(prediction_results.inputs.snapshot(i).mean))
+        print('\tx = {}'.format(prediction_results.states.snapshot(i).mean))
+        print('\tz = {}'.format(prediction_results.outputs.snapshot(i).mean))
+        print('\tevent state = {}'.format(prediction_results.event_states.snapshot(i).mean))
 
-    print('\nToE:', toe.mean)
+    print('\nToE:', prediction_results.time_of_event.mean)
 
     # You can also access the final state (of type UncertainData), like so:
-    final_state = toe.final_state
+    final_state = prediction_results.time_of_event.final_state
     print('Final state @EOD: ', final_state['EOD'].mean)
     # toe.plot_hist()
     # plt.show()

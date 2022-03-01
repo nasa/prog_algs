b876cc8a7ff653e1ce2c465ae9e05ada19e198cb
Lawrence Hwang <lawrenceh326@gmail.com>
2022-03-01 14:02:45 -0800

    Tutorials test file created

---
+++ tests/test_tutorials.py
@@ -0,0 +1,7 @@
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+
+import unittest
+import papermill
+
+class TestTutorials(unittest.TestCase):
+    pass

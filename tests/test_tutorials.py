92e0732d9e7db0973bcbb5bd0aa7a54b79d2378d
Lawrence Hwang <lawrenceh326@gmail.com>
2022-03-02 15:32:45 -0800

    Only test execution and major variables

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -5,28 +5,30 @@ from testbook import testbook
 
 class TestTutorials(unittest.TestCase):
     def test_tutorial_ipynb(self):
-        with testbook('./tutorial.ipynb', execute=False) as tb:
-            # TESTING STATE ESTIMATION
-            # Test importing and initializing BatteryCircuit, UnscentedKalmanFilter
-            tb.execute_cell([0,1,2,3,4,5,6])
-            self.assertEqual(tb.ref("m")._type, "BatteryCircuit")
-            self.assertEqual(tb.ref("x0"), {'tb': 18.95, 'qb': 7856.3254, 'qcp': 0, 'qcs': 0})
-            self.assertEqual(tb.ref("est")._type, "UnscentedKalmanFilter")
-            # Test estimating the system state; compare against expected print output
-            tb.execute_cell([7,8])
-            state_estimation_prints = [line.strip() for line in tb.cell_output_text(8).splitlines()]
-            self.assertEqual(state_estimation_prints[0], "Prior State: {'tb': 18.95, 'qb': 7856.3254, 'qcp': 0.0, 'qcs': 0.0}")
-            self.assertEqual(state_estimation_prints[1], "SOC:  1.0")
-            self.assertEqual(state_estimation_prints[2], "Posterior State: {'tb': 20.15440859119512, 'qb': 7856.125354781064, 'qcp': 0.20143751745290445, 'qcs': 0.20014829013698857}")
-            self.assertEqual(state_estimation_prints[3], "SOC:  0.9999742773281554")
-            self.assertEqual(state_estimation_prints[4], "<Figure size 1000x900 with 9 Axes><Figure size 1000x900 with 9 Axes>")
+        with testbook('./tutorial.ipynb', execute=True) as tb:
+            # # TESTING STATE ESTIMATION
+            # # Test importing and initializing BatteryCircuit, UnscentedKalmanFilter
+            # tb.execute_cell([0,1,2,3,4,5,6])
+            # self.assertEqual(tb.ref("m")._type, "BatteryCircuit")
+            # self.assertEqual(tb.ref("x0"), {'tb': 18.95, 'qb': 7856.3254, 'qcp': 0, 'qcs': 0})
+            # self.assertEqual(tb.ref("est")._type, "UnscentedKalmanFilter")
+            # # Test estimating the system state; compare against expected print output
+            # tb.execute_cell([7,8])
+            # state_estimation_prints = [line.strip() for line in tb.cell_output_text(8).splitlines()]
+            # self.assertEqual(state_estimation_prints[0], "Prior State: {'tb': 18.95, 'qb': 7856.3254, 'qcp': 0.0, 'qcs': 0.0}")
+            # self.assertEqual(state_estimation_prints[1], "SOC:  1.0")
+            # self.assertEqual(state_estimation_prints[2], "Posterior State: {'tb': 20.15440859119512, 'qb': 7856.125354781064, 'qcp': 0.20143751745290445, 'qcs': 0.20014829013698857}")
+            # self.assertEqual(state_estimation_prints[3], "SOC:  0.9999742773281554")
+            # self.assertEqual(state_estimation_prints[4], "<Figure size 1000x900 with 9 Axes><Figure size 1000x900 with 9 Axes>") # Generating figure object
 
-            # Expected STDOUT
-            # Prior State: {'tb': 18.95, 'qb': 7856.3254, 'qcp': 0.0, 'qcs': 0.0} 
-            # SOC:  1.0
-            # Posterior State: {'tb': 20.153040257821697, 'qb': 7854.124938401348, 'qcp': 2.2004631247958346, 'qcs': 2.200499193776155}
-	        # SOC:  0.9997170552142662
-            
+            # # TESTING STATE ESTIMATION
+            # # Test importing and initializing MonteCarlo
+            # tb.execute_cell([9,10,11,12,13,14])
+            # tb.ref("mc")
+
+            # TESTING TO ENSURE EXECUTION WITHOUT FAIL
+            self.assertEqual(tb.ref("m")._type, "BatteryCircuit")
+            self.assertEqual(tb.ref("est")._type, "UnscentedKalmanFilter")
 
 def run_tests():
     l = unittest.TestLoader()

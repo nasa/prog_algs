17bfeb9e59ec382011dc446eea12d8b25cee897f
Lawrence Hwang <lawrenceh326@gmail.com>
2022-03-10 15:45:18 -0800

    Passing samples; x0 missing state

--- tests/test_state_estimators.py
+++ tests/test_state_estimators.py
@@ -102,16 +102,16 @@ class TestStateEstimators(unittest.TestCase):
         # Test UnscentedKalmanFilter MultivariateNormalDist
         # from prog_algs.uncertain_data.multivariate_normal_dist import MultivariateNormalDist
         # x_mvnd = MultivariateNormalDist({'x': 1.75, 'v': 35}) # NEED ARGUMENTS
-        # filt_mvnd = MultivariateNormalDist(m, x_mvnd)
+        # filt_mvnd = UnscentedKalmanFilter(m, x_mvnd)
         # self.assertDictEqual(filt_mvnd.x.mean, x_mvnd.mean)
         # self.assertTrue((filt_mvnd.x.cov == x_mvnd.cov).all())
 
         # Test UnscentedKalmanFilter UnweightedSamples
-        # from prog_algs.uncertain_data.unweighted_samples import UnweightedSamples
-        # x_us = UnweightedSamples({'x': 1.75, 'v': 35}) # NEED ARGUMENTS
-        # filt_us = UnweightedSamples(m, x_us)
-        # self.assertDictEqual(filt_us.x.mean, x_us.mean)
-        # self.assertTrue((filt_us.x.cov == x_us.cov).all())
+        from prog_algs.uncertain_data.unweighted_samples import UnweightedSamples
+        x_us = UnweightedSamples([{'a': 1, 'b':2}, {'a': 3, 'b':-2}])
+        filt_us = UnscentedKalmanFilter(m, x_us)
+        self.assertDictEqual(filt_us.x.mean, x_us.mean)
+        self.assertTrue((filt_us.x.cov == x_us.cov).all())
    
     def __incorrect_input_tests(self, filter):
         class IncompleteModel:

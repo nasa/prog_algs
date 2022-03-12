2c70c64c71cedb839bcc9dfe9d92a258d724aa3b
Lawrence Hwang <lawrenceh326@gmail.com>
2022-03-11 16:39:11 -0800

    Replace sd in UncertainData with samples method

--- src/prog_algs/state_estimators/particle_filter.py
+++ src/prog_algs/state_estimators/particle_filter.py
@@ -64,13 +64,13 @@ class ParticleFilter(state_estimator.StateEstimator):
             elif isinstance(self.parameters['x0_uncertainty'], Number):
                 sd = array([self.parameters['x0_uncertainty']] * len(x0))
             samples = [random.normal(x[i], sd[i], self.parameters['num_particles']) for i in range(len(x))]
-            self.particles = dict(zip(x0.keys(), samples))
         elif isinstance(x0, UncertainData):
             x = array(list(x0.mean.values()))
             sd = None # Using sampling method of UncertainData
-            # samples = 
+            samples = x0.sample()
         else:
             raise ProgAlgTypeError("ProgAlgTypeError: x0 must be of type {{UncertainData}} or x0_uncertainty must be of type {{dict, Number}}.")
+        self.particles = dict(zip(x0.keys(), samples))
 
         if 'R' in self.parameters:
             # For backwards compatibility

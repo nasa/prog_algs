181670842e9b6399a1cd321feba58a0838d91436
Matteo Corbetta <matteo.corbetta@nasa.gov>
2022-03-16 15:59:41 -0700

    fixed but in line 102 of particle_filter where process noise was added with dt=1, while now dt is passed to the apply_process_noise function correctly.

--- src/prog_algs/state_estimators/particle_filter.py
+++ src/prog_algs/state_estimators/particle_filter.py
@@ -9,6 +9,8 @@ from ..uncertain_data import UnweightedSamples
 from ..exceptions import ProgAlgTypeError
 from warnings import warn
 
+import matplotlib.pyplot as plt
+import numpy as np
 
 class ParticleFilter(state_estimator.StateEstimator):
     """
@@ -97,7 +99,7 @@ class ParticleFilter(state_estimator.StateEstimator):
 
         if self.model.is_vectorized:
             # Propagate particles state
-            self.particles = apply_process_noise(next_state(particles, u, dt))
+            self.particles = apply_process_noise(next_state(particles, u, dt), dt)
 
             # Get particle measurements
             zPredicted = apply_measurement_noise(output(self.particles))
@@ -133,7 +135,7 @@ class ParticleFilter(state_estimator.StateEstimator):
 
         # Convert to weights
         unnorm_weights = exp(scaled_weights)
-
+        
         # Normalize
         total_weight = sum(unnorm_weights)
         self.weights = unnorm_weights / total_weight

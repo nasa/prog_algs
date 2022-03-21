44b5260f54c4fb06969790fff2f626c2d86d484c
Lawrence Hwang <lawrenceh326@gmail.com>
2022-03-21 12:31:20 -0700

    Restoring PF test

--- tests/test_state_estimators.py
+++ tests/test_state_estimators.py
@@ -143,31 +143,31 @@ class TestStateEstimators(unittest.TestCase):
 
         filt = ParticleFilter(m, x_guess)
         self.__test_state_est(filt, m)
-        # m = MockProgModel(process_noise=5e-2, measurement_noise=0)
-        # x0 = m.initialize()
-        # filt = ParticleFilter(m, x0, n_samples=200, x0_uncertainty=0.1)
-        # self.assertTrue(all(key in filt.x[0] for key in m.states))
-        # # self.assertDictEqual(x0, filt.x) // Not true - sample production means they may not be equal
-        # u = {'i1': 1, 'i2': 2}
-        # x = m.next_state(m.initialize(), u, 0.1)
-        # filt.estimate(0.1, u, m.output(x))  
-        # x_est = filt.x.mean
-        # self.assertFalse( x0 == x_est )
-        # self.assertFalse( {'a': 1.1, 'b': 2, 'c': -5.2} == x_est )
+        m = MockProgModel(process_noise=5e-2, measurement_noise=0)
+        x0 = m.initialize()
+        filt = ParticleFilter(m, x0, n_samples=200, x0_uncertainty=0.1)
+        self.assertTrue(all(key in filt.x[0] for key in m.states))
+        # self.assertDictEqual(x0, filt.x) // Not true - sample production means they may not be equal
+        u = {'i1': 1, 'i2': 2}
+        x = m.next_state(m.initialize(), u, 0.1)
+        filt.estimate(0.1, u, m.output(x))  
+        x_est = filt.x.mean
+        self.assertFalse( x0 == x_est )
+        self.assertFalse( {'a': 1.1, 'b': 2, 'c': -5.2} == x_est )
 
-        # # Between the model and sense outputs
-        # o_est = m.output(x_est)
-        # o0 = m.output(x0)
-        # self.assertGreater(o_est['o1'], 0.7) # Should be between 0.9-o0['o1'], choosing this gives some buffer for noise
-        # self.assertLess(o_est['o1'], o0['o1']) # Should be between 0.8-0.9, choosing this gives some buffer for noise. Testing that the estimate is improving
+        # Between the model and sense outputs
+        o_est = m.output(x_est)
+        o0 = m.output(x0)
+        self.assertGreater(o_est['o1'], 0.7) # Should be between 0.9-o0['o1'], choosing this gives some buffer for noise
+        self.assertLess(o_est['o1'], o0['o1']) # Should be between 0.8-0.9, choosing this gives some buffer for noise. Testing that the estimate is improving
 
-        # with self.assertRaises(Exception):
-        #     # Only given half of the inputs 
-        #     filt.estimate(0.5, {}, {'o1': -2.0})
+        with self.assertRaises(Exception):
+            # Only given half of the inputs 
+            filt.estimate(0.5, {}, {'o1': -2.0})
 
-        # with self.assertRaises(Exception):
-        #     # Missing output
-        #     filt.estimate(0.5, {'i1': 0, 'i2': 0}, {})
+        with self.assertRaises(Exception):
+            # Missing output
+            filt.estimate(0.5, {'i1': 0, 'i2': 0}, {})
 
     def test_measurement_eq_UKF(self):
         class MockProgModel2(MockProgModel):
@@ -274,7 +274,7 @@ class TestStateEstimators(unittest.TestCase):
 
         m = ThrownObject(process_noise=5e-2, measurement_noise=5e-2)
         x = m.initialize()
-         # Test KalmanFilter ScalarData
+        # Test KalmanFilter ScalarData
         from prog_algs.uncertain_data.scalar_data import ScalarData
         x_scalar = ScalarData({'x': 1.75, 'v': 35})
         filt_scalar = KalmanFilter(m, x_scalar)

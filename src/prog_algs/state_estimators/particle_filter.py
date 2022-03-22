ae806fb11a61572d2f7bd4c5e0496d33a68258bf
Lawrence Hwang <lawrenceh326@gmail.com>
2022-03-22 14:17:26 -0700

    Catch sd variable assignment in PF

--- src/prog_algs/state_estimators/particle_filter.py
+++ src/prog_algs/state_estimators/particle_filter.py
@@ -68,6 +68,8 @@ class ParticleFilter(state_estimator.StateEstimator):
                 sd = array([self.parameters['x0_uncertainty'][key] for key in x0.keys()])
             elif isinstance(self.parameters['x0_uncertainty'], Number):
                 sd = array([self.parameters['x0_uncertainty']] * len(x0))
+            else:
+                raise ProgAlgTypeError("ProgAlgTypeError: x0_uncertainty must be of type {{dict, Number}}.")
             samples = [random.normal(x[i], sd[i], self.parameters['num_particles']) for i in range(len(x))]
         else:
             raise ProgAlgTypeError("ProgAlgTypeError: x0 must be of type {{UncertainData}} or x0_uncertainty must be of type {{dict, Number}}.")

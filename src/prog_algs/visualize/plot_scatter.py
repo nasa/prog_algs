88a68856a0dc11d74b99935ff7dae071bb654df3
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-01 20:26:40 -0700

    Type hints for plot_scatter

--- src/prog_algs/visualize/plot_scatter.py
+++ src/prog_algs/visualize/plot_scatter.py
@@ -1,9 +1,12 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 from matplotlib.collections import PathCollection
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from math import sqrt
 
-def plot_scatter(samples, fig = None, keys = None, legend = 'auto', **kwargs):
+from numpy import array
+
+def plot_scatter(samples : array(dict), fig : Figure = None, keys : list(str) = None, legend : str = 'auto', **kwargs) -> Figure:
     """
     Produce a scatter plot for a given list of states
 

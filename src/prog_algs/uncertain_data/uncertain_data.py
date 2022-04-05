bdb169e9877e682986b1251dd1150e6e4a862a39
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-05 11:46:18 -0700

    Update uncertain_data.py

--- src/prog_algs/uncertain_data/uncertain_data.py
+++ src/prog_algs/uncertain_data/uncertain_data.py
@@ -70,7 +70,7 @@ class UncertainData(ABC):
         """Calculate percentage of dist is within specified bounds
 
         Args:
-            bounds ([float, float]): Lower and upper bounds
+            bounds ((float, float)): Lower and upper bounds
             keys (list of strings, optional): Keys to analyze. Defaults to all keys.
 
         Returns:

f13a0a4cf052d955f561ae05c25243484d307ca9
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-04 19:32:25 -0700

    Update unweighted_samples.py

--- src/prog_algs/uncertain_data/unweighted_samples.py
+++ src/prog_algs/uncertain_data/unweighted_samples.py
@@ -29,7 +29,7 @@ class UnweightedSamples(UncertainData, UserList):
             # is in form of [{key: value, ...}, ...]
             self.data = samples
         else:
-            raise ValueError('ValueError: Invalid input. Must be list or dict, was {}'.format(type(samples)))
+            raise ValueError('Invalid input. Must be list or dict, was {}'.format(type(samples)))
 
     def sample(self, num_samples = 1, replace = True):
         # Completely random resample
@@ -112,7 +112,7 @@ class UnweightedSamples(UncertainData, UserList):
         if isinstance(bounds, list):
             bounds = {key: bounds for key in self.keys()}
         if not isinstance(bounds, dict) or all([isinstance(b, list) and len(b) == 2 for b in bounds]):
-            raise TypeError("TypeError: Bounds must be list [lower, upper] or dict (key: [lower, upper]), was {}".format(type(bounds)))
+            raise TypeError("Bounds must be list [lower, upper] or dict (key: [lower, upper]), was {}".format(type(bounds)))
         n_elements = len(self.data)
         return {key: sum([x is not None and x < bounds[key][1] and x > bounds[key][0] for x in self.key(key)])/n_elements for key in keys}
 

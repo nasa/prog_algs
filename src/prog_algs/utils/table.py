42bab026f2307e8f27d20a49be2b7328afb3c14a
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-06 16:33:05 -0700

    Setup init, print in table

--- src/prog_algs/utils/table.py
+++ src/prog_algs/utils/table.py
@@ -1,6 +1,17 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the
 # National Aeronautics and Space Administration.  All Rights Reserved.
 
+from collections import defaultdict
+
+
 class Table():
-    pass
+    result = []
+    column_lengths = defaultdict(int)
+
+    def __init__(self, input_dict : dict):
+        pass
+
+    def print(self, print_flag : bool = True) -> None:
+        if print_flag:
+            print(*self.result, sep = "\n")
 

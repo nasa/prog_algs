8076ef2518275712915e870bef0165b82f1220ea
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-11 02:00:15 -0700

    Iterative table print as function

--- src/prog_algs/utils/table.py
+++ src/prog_algs/utils/table.py
@@ -48,7 +48,68 @@ def print_table_iterative(input_dict : dict, title : str, print_flag : bool = Tr
     print_flag : bool = True
         An optional boolean value determining whether the generated table is printed.
     """
-    pass
+    def sub_print_table_iterative(input_dict : dict, title : str = None, print_flag : bool = True):
+        res = []
+        col_name_row = "|"
+        value_row = "|"
+        title = title
+        for k,v in input_dict.items():
+            col_len = len(max(str(k), str(v))) + 2
+            col_name_row += f"{str(k):^{col_len}}|"
+            value_row += f"{str(v):^{col_len}}|"
+
+        break_row = "+{}+".format((len(col_name_row)-2)*'-')
+        res = [break_row, col_name_row, break_row, value_row, break_row]
+        title = f"+{title:^{len(break_row)-2}}+"
+            
+        if title:
+            print(title)
+        if print_flag:
+            print(*res, sep = "\n")
+
+    result = []
+    sub_result = []
+    column_lengths = defaultdict(int)
+
+    for m in input_dict.keys():
+        if column_lengths["key"] < len(m):
+            column_lengths["key"] = max(len(m), len("key")+2) # +2 for header name spacing; less cramped view
+        for k,v in input_dict[m].items():
+            if isinstance(v, dict):
+                column_lengths[k] = None
+            else:     
+                update_len = max(len(str(v)),len(k)+2)
+                if column_lengths[k] < update_len:
+                    column_lengths[k] = update_len
+
+    # Formatting header and columns
+    col_name_row = "|"
+    for k in column_lengths.keys(): # Using key order because they shouldn't change while printing
+        # if k in self.input_dict and not isinstance(self.input_dict[k], dict):
+        if column_lengths[k]:
+            col_name_row += f"{k:^{column_lengths[k]}}|"
+    break_row = "+{}+".format((len(col_name_row)-2)*'-')
+    result = [break_row, col_name_row, break_row]
+    title = f"+{title:^{len(break_row)-2}}+"
+
+    for m in input_dict:
+        metric_row = f"|{m:^{column_lengths['key']}}|" 
+        for k,v in input_dict[m].items():
+            # check isinstance v
+            if isinstance(v, dict):
+                sub_result.append(sub_print_table_iterative(v, f"{m} {k}"))
+            # else:
+            if column_lengths[k]:
+                metric_row += f"{str(v):^{column_lengths[k]}}|"
+        result.extend([metric_row, break_row])
+
+    if title:
+        print(title)
+    if print_flag:
+        print(*result, sep = "\n")
+    if sub_result:
+        for t in sub_result:
+            t.print()
 
 class Table():
     """

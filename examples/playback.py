28049e6bdeb64f03c276097761fd4af4366394b8
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-18 16:12:06 -0700

    Logic adjusted

--- examples/playback.py
+++ examples/playback.py
@@ -130,22 +130,20 @@ def run_example():
             """
             
             # Set an alpha value
-            alpha_bounds = 0.10 # set the alpha value percentage here; for x - a, x + a
-            beta_percentage = 0.60 # set the beta value percentage here; B% of distribution between alpha bounds
-            result = {}
+            alpha_bounds = 0.05 # set the alpha value percentage here; for x - a, x + a
+            beta_percentage = 0.90 # set the beta value percentage here; B% of distribution between alpha bounds
+            bounds = {}
             for key, value in ground_truth_tte.items():
                 # Set bounds for precentage_in_bounds by adding/subtracting to the ground_truth
                 alpha_calc = value * alpha_bounds
-                bounds = {key: [value - alpha_calc, value + alpha_calc]} # Construct bounds for all events
-                percentage_in_bounds = tte.percentage_in_bounds(bounds)
-
-                # Verify if percentage in bounds for this ground truth meets beta distribution percentage limit
-                # result[key] = (sum(percentage_in_bounds.values()) / len(percentage_in_bounds.keys())) > beta_percentage
-                result[key] = percentage_in_bounds[key] > beta_percentage 
-            return result
+                bounds[key] = [value - alpha_calc, value + alpha_calc] # Construct bounds for all events
+            percentage_in_bounds = tte.percentage_in_bounds(bounds)
+            
+            # Verify if percentage in bounds for this ground truth meets beta distribution percentage limit
+            return {key: percentage_in_bounds[key] > beta_percentage for key in percentage_in_bounds.keys()}
 
         ph = prognostic_horizon(profile, criteria_eqn, ground_truth)
-        print(f"Prognostic Horizon for EOD: {ph['EOD']}")
+        print(f"Prognostic Horizon for 'EOD': {ph['EOD']}")
 
     input('Press any key to exit')
 

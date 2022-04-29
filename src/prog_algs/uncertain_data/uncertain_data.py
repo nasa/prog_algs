3c4a48a58637b05d10d3bf7680d1a14c2314ef05
Lawrence Hwang <lawrenceh326@gmail.com>
2022-04-29 15:42:26 -0700

    Type checking logic fix + Import fix

--- src/prog_algs/uncertain_data/uncertain_data.py
+++ src/prog_algs/uncertain_data/uncertain_data.py
@@ -7,7 +7,7 @@ from numpy import array
 
 from ..utils.table import print_table_recursive
 from ..visualize import plot_scatter, plot_hist
-from prog_models.utils import DictLikeMatrixWrapper
+from prog_models.utils.containers import DictLikeMatrixWrapper
 
 
 class UncertainData(ABC):
@@ -72,7 +72,7 @@ class UncertainData(ABC):
             dict(str:float): Relative accuracy for each event where value is relative accuracy between [0,1]
         """
         # if this check isn't here, goes to divide by zero check and raises AttributeError instead of TypeError. Keep? There are unittests checking for type
-        if (not isinstance(ground_truth, dict)) or (not isinstance(ground_truth, DictLikeMatrixWrapper)):
+        if (not isinstance(ground_truth, dict)) and (not isinstance(ground_truth, DictLikeMatrixWrapper)):
             raise TypeError("Ground truth must be passed as a dictionary or *.container argument.")
         if not all(ground_truth.values()):
             raise ZeroDivisionError("Ground truth values must be non-zero in calculating relative accuracy.")

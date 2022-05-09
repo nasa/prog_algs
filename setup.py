76bb56f25ec19fa60416f8a9bd766a5c725bf4dd
Lawrence Hwang <lawrenceh326@gmail.com>
2022-05-09 12:55:03 -0700

    Adding copyright for setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# Copyright © 2022 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+
 from setuptools import setup, find_packages
 import pathlib
 

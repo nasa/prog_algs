d6044a282f3ff6d3b8bf95b594adefc0027f8eeb
Lawrence Hwang <lawrenceh326@gmail.com>
2022-05-10 10:36:41 -0700

    Correcting co to 2021

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright © 2022 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
 
 from setuptools import setup, find_packages
 import pathlib

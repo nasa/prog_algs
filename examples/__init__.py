8173c075675d6af30774e41160ca8d3dea26404d
Christopher Teubert <christopher.a.teubert@nasa.gov>
2022-10-04 11:59:29 -0700

    Update examples

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,3 +1,3 @@
 # Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration. All Rights Reserved.
 
-__all__ = ['basic_example', 'benchmarking_example', 'eol_event', 'horizon', 'kalman_filter', 'utpredictor', 'new_state_estimator_example', 'measurement_eqn_example', 'playback', 'predict_specific_event', 'thrown_object_example']
+__all__ = ['basic_example', 'basic_example_battery', 'benchmarking_example', 'eol_event', 'horizon', 'kalman_filter', 'new_state_estimator_example', 'measurement_eqn_example', 'playback', 'predict_specific_event']
